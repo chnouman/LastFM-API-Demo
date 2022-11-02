059a86dcedbb6a7cf003388001bb3ceaea2411a9
chnouman <chnouman200@gmail.com>
2022-11-02 21:27:49 +0400

    Added tests for AlbumDao

--- app/src/androidTest/java/com/chnouman/lastfmapidemo/data/local/LastFMDatabaseTest.kt
+++ app/src/androidTest/java/com/chnouman/lastfmapidemo/data/local/LastFMDatabaseTest.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalCoroutinesApi::class)
+
 package com.chnouman.lastfmapidemo.data.local
 
 import android.arch.core.executor.testing.InstantTaskExecutorRule
@@ -6,7 +8,10 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.filters.SmallTest
 import androidx.test.runner.AndroidJUnit4
 import com.chnouman.lastfmapidemo.data.local.dao.AlbumDao
+import com.chnouman.lastfmapidemo.data.local.entities.Album
 import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -33,11 +38,90 @@ class LastFMDatabaseTest {
     }
 
     @Test
-    fun test(){
+    fun basicTest(){
 
         assertThat(database).isNotNull()
         assertThat(dao).isNotNull()
     }
+
+    @Test
+    fun insertShoppingItemSuccess() = runTest {
+        val shoppingItem = Album("ABC",0,"https://lastfm.com","https://lastfm.com","ABC",false)
+        dao.insert(shoppingItem)
+        assertThat(dao.getAll()).hasSize(1)
+    }
+    @Test
+    fun insertShoppingItemFailure() = runTest {
+        val shoppingItem = Album("ABC",0,"https://lastfm.com","https://lastfm.com","ABC",false)
+        val shoppingItem2 = Album("ABC2",0,"https://lastfm.com","https://lastfm.com","ABC",false)
+        dao.insert(shoppingItem)
+        dao.insert(shoppingItem2)
+        assertThat(dao.getAll().size).isEqualTo(2)
+        assertThat(dao.getAll()).isNotEqualTo(1)
+        assertThat(dao.getAll().size).isNotEqualTo(3)
+    }
+
+    @Test
+    fun duplicateScenarioSuccess() = runTest {
+        val shoppingItem = Album("ABC",0,"https://lastfm.com","https://lastfm.com","ABC",false)
+        dao.insert(shoppingItem)
+        dao.insert(shoppingItem)
+        assertThat(dao.getAll().size).isEqualTo(1)
+     }
+
+    @Test
+    fun duplicateScenarioFailure() = runTest {
+        val shoppingItem = Album("ABC",0,"https://lastfm.com","https://lastfm.com","ABC",false)
+        dao.insert(shoppingItem)
+        dao.insert(shoppingItem)
+        dao.insert(shoppingItem)
+        assertThat(dao.getAll().size).isNotEqualTo(3)
+        assertThat(dao.getAll().size).isNotEqualTo(2)
+        assertThat(dao.getAll().size).isEqualTo(1)
+     }
+
+
+    @Test
+    fun deleteScenarioSuccess() = runTest {
+        val shoppingItem = Album("ABC",0,"https://lastfm.com","https://lastfm.com","ABC",false)
+        dao.insert(shoppingItem)
+        dao.delete(shoppingItem.name)
+        assertThat(dao.getAll().size).isEqualTo(0)
+     }
+
+    @Test
+    fun deleteScenarioFailure() = runTest {
+        val shoppingItem = Album("ABC",0,"https://lastfm.com","https://lastfm.com","ABC_ARTIST",false)
+        dao.insert(shoppingItem)
+        dao.delete(shoppingItem.artistName)//Passing Wrong Name
+        assertThat(dao.getAll().size).isEqualTo(1)
+        assertThat(dao.getAll().size).isNotEqualTo(0)
+     }
+
+    @Test
+    fun insertAllScenarioTest() = runTest {
+        val shoppingItem = Album("ABC",0,"https://lastfm.com","https://lastfm.com","ABC_ARTIST",false)
+        val shoppingItem2 = Album("ABC2",0,"https://lastfm.com","https://lastfm.com","ABC_ARTIST",false)
+        dao.insertAll(mutableListOf(shoppingItem,shoppingItem2))
+        assertThat(dao.getAll().size).isEqualTo(2)
+     }
+
+    @Test
+    fun isExistScenarioSuccess() = runTest {
+        val shoppingItem = Album("ABC",0,"https://lastfm.com","https://lastfm.com","ABC_ARTIST",false)
+        dao.insert(shoppingItem)
+        val exist = dao.isExist(shoppingItem.name)
+        assertThat(exist).isTrue()
+     }
+
+    @Test
+    fun isExistScenarioFailure() = runTest {
+        val shoppingItem = Album("ABC",0,"https://lastfm.com","https://lastfm.com","ABC_ARTIST",false)
+        dao.insert(shoppingItem)
+        dao.delete(shoppingItem.name)
+        val exist = dao.isExist(shoppingItem.name)
+        assertThat(exist).isFalse()
+     }
 
     @After
     fun teardown() {
