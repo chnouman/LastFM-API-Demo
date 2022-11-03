61552d2c1f9430349765ace4835e208724aa772e
chnouman <chnouman200@gmail.com>
2022-11-03 09:46:29 +0400

    Added domain layer setup for repository

---
+++ app/src/main/java/com/chnouman/lastfmapidemo/domain/Repository.kt
@@ -0,0 +1,23 @@
+package com.chnouman.lastfmapidemo.domain
+
+import androidx.paging.PagingSource
+import com.chnouman.lastfmapidemo.core.util.Resource
+import com.chnouman.lastfmapidemo.data.local.entities.Album
+import com.chnouman.lastfmapidemo.data.local.entities.Track
+import com.chnouman.lastfmapidemo.data.remote.models.searchartist.Artist
+import kotlinx.coroutines.flow.Flow
+
+interface Repository {
+    fun searchArtist(query: String, apiKey: String): Flow<Resource<MutableList<Artist>>>
+    fun getTopAlbums(query: String, apiKey: String): Flow<Resource<MutableList<Album>>>
+    fun getAlbumInfo(artist: String, album: String, apiKey: String): Flow<Resource<Track>>
+    fun getLocalAlbums(): PagingSource<Int, Album>
+    fun compareLocalAlbums(albums: MutableList<Album>): MutableList<Album>
+    fun getLocalTracks(albumName: String): Flow<Resource<MutableList<Track>>>
+    fun addAlbumDto(albumsDto: Album)
+    fun addTracks(tracks: MutableList<Track>)
+    fun addArtist(artist: com.chnouman.lastfmapidemo.data.local.entities.Artist)
+    fun deleteAlbum(albumsDto: Album)
+    fun deleteTracks(albumsName: String)
+    fun deleteArtist(artist: String)
+}
