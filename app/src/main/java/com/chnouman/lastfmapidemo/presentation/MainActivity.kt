d83eeb62941c3928898d7c11c5d5404ffba02fe3
chnouman <chnouman200@gmail.com>
2022-11-03 21:00:19 +0400

    Added Delegate for toolbar

--- app/src/main/java/com/chnouman/lastfmapidemo/presentation/MainActivity.kt
+++ app/src/main/java/com/chnouman/lastfmapidemo/presentation/MainActivity.kt
@@ -3,28 +3,21 @@ package com.chnouman.lastfmapidemo.presentation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupWithNavController
 import com.chnouman.lastfmapidemo.R
 import com.chnouman.lastfmapidemo.databinding.ActivityMainBinding
+import com.chnouman.lastfmapidemo.presentation.delegate.ToolbarDelegate
+import com.chnouman.lastfmapidemo.presentation.delegate.ToolbarDelegateImpl
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ToolbarDelegate by ToolbarDelegateImpl() {
 
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        setSupportActionBar(binding.toolbar)
-
-        val config = AppBarConfiguration(findNavController(R.id.navHostFragment).graph)
-        binding.toolbar.setupWithNavController(
-            findNavController(R.id.navHostFragment),
-            config
-        )
+        setupToolbar(this, binding.toolbar, findNavController(R.id.navHostFragment))
     }
 }
