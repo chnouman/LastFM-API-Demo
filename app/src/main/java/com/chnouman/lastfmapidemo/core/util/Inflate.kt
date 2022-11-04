1ca71d2ec2dc5fb42cc5dfb9bc9dac0c2b6bc641
chnouman <chnouman200@gmail.com>
2022-11-04 16:47:52 +0400

    Added extensions

---
+++ app/src/main/java/com/chnouman/lastfmapidemo/core/util/Inflate.kt
@@ -0,0 +1,5 @@
+package com.chnouman.lastfmapidemo.core.util
+
+import android.view.LayoutInflater
+
+typealias Inflate<T> = (LayoutInflater) -> T
---
+++ app/src/main/java/com/chnouman/lastfmapidemo/core/util/extensions/StringExtensions.kt
@@ -0,0 +1,3 @@
+package com.chnouman.lastfmapidemo.core.util.extensions
+
+val String.Companion.empty: String get() = ""
