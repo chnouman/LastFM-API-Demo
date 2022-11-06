b74b3b2a894f9a7396c6b40c2dec84fc67f79dc5
chnouman <chnouman200@gmail.com>
2022-11-06 17:43:11 +0400

    Updated PagingSource

--- app/src/main/java/com/chnouman/lastfmapidemo/presentation/search/paging/LastFMPagingSource.kt
+++ app/src/main/java/com/chnouman/lastfmapidemo/presentation/search/paging/LastFMPagingSource.kt
@@ -11,7 +11,10 @@ class LastFMPagingSource(
     private val searchArtist: SearchArtist
 ) : PagingSource<Int, Artist>() {
     override fun getRefreshKey(state: PagingState<Int, Artist>): Int? {
-        return null
+        return state.anchorPosition?.let { anchorPosition ->
+            val anchorPageIndex = state.pages.indexOf(state.closestPageToPosition(anchorPosition))
+            state.pages.getOrNull(anchorPageIndex + 1)?.prevKey ?: state.pages.getOrNull(anchorPageIndex - 1)?.nextKey
+        }
     }
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Artist> {
